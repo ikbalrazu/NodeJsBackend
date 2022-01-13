11dd4e58416d445016d941703c3d3811878a4cb3
ikbalrazu <iqbalraju123@gmail.com>
2022-01-13 16:48:24 +0600

    add cors in route

--- server.js
+++ server.js
@@ -1,9 +1,10 @@
 const express = require('express');
+const cors = require('cors');
 
 const app = express();
 const port = process.env.PORT || 3001;
 
-app.get("/api",(req,res)=>{
+app.get("/api",cors(),(req,res)=>{
     res.json({"users":["iqbal","raju","riaz","sharif"]})
 })
 
